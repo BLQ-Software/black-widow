3260df730f1554d9f76da3df795157a4e6feef50
dqu123 <davidqu12345@gmail.com>
2015-10-02 22:48:31 -0700

    Added argparser to run_simulator.py

--- src/run_simulator.py
+++ src/run_simulator.py
@@ -1,3 +1,17 @@
+"""Runs blackwidow simulator on a specified set of files.
+
+This script parses user arguments and configures the blackwidow
+module to run based on user arguments.
+"""
+import argparse
 import blackwidow
 
-blackwidow.run('case1.txt')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run a network simulation.')
+    parser.add_argument('files', metavar='config_file', type=str, nargs='+',
+                        help='a file to process')
+    args = parser.parse_args()
+
+    # Iterate through config files specified.
+    for f in args.files:
+        blackwidow.run(f)
