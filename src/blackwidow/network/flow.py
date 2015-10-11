376e4df1eff3417e1bbffcb0b8debec69db92839
Justin Leong <justinleong16@gmail.com>
2015-10-11 12:52:21 -0700

    Adding flow method

---
+++ src/blackwidow/network/flow.py
@@ -0,0 +1,15 @@
+class Flow:
+    """Simple class for flows.
+    
+    Flows will trigger host behavior.
+    """
+    def __init__(self, env, name, source, destination, amount):
+        """Constructor for Flow class.
+        """
+        self.source = source
+        self.destination = destination
+        self.amount = amount
+
+    def send(): 
+        """Connects to a link"""
+
