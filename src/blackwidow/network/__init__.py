76597e896954f6d0f06e92b7e58de4fbd7a56a0f
dqu123 <davidqu12345@gmail.com>
2015-10-16 23:12:15 -0700

    Changed network format to use dictionaries and a unique id constraint.

--- src/blackwidow/network/__init__.py
+++ src/blackwidow/network/__init__.py
@@ -4,12 +4,25 @@ from blackwidow.network.router import Router
 from blackwidow.network.flow import Flow
 
 class Network():
-    """Python representation of the network."""
+    """Python representation of the network.
+    
+    Each host, router, link, and flow object is denoted by a 
+    unique character id, and placed in a distinct dictionary.
+    The check_id function checks the unique id constraint before
+    construction any new objects. This is a global id constraint
+    across all objects.
+    """
     def __init__(self):
-        self.hosts = []
-        self.routers = []
-        self.links = []
-        self.flows = []
+        self.hosts = {}
+        self.routers = {}
+        self.links = {}
+        self.flows = {}
+
+    def check_id(self, obj_id):
+        """Raise an exception if object id is not unique."""
+        if ((obj_id in self.hosts) or (obj_id in self.routers) or
+                (obj_id in self.links) or (obj_id in self.flows)):
+            raise NameError('id {0} already exists.'.format(obj_id))
 
     def dump(self):
         """Prints out network"""
@@ -20,7 +33,8 @@ class Network():
 
     def add_host(self, host_id):
         """Construct host and add to list of hosts."""
-        self.hosts.append(Host(host_id))
+        self.check_id(host_id)
+        self.hosts[host_id] = Host(host_id)
 
     def add_router(self, router_id):
         pass
--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -3,4 +3,4 @@ from blackwidow.parser import config
 def test_host():
     network = config('case0.json')
     network.dump()
-    assert network.hosts[0].host_id == 'H1'
+    assert network.hosts['H1'].host_id == 'H1'
