b052591464466a0d691bf8af2ac3abee266cbe4c
Justin Leong <justinleong16@gmail.com>
2015-10-23 23:56:10 -0700

    Added time to flow.

--- src/blackwidow/network/flow.py
+++ src/blackwidow/network/flow.py
@@ -1,15 +1,14 @@
 """ Flow class
 """
 
-# from blackwidow.network.host import Host
-from blackwidow.network.packet import DataPacket
-from blackwidow.network.packet import AckPacket
+from blackwidow.network import network
+from blackwidow.network.packet import AckPacket, DataPacket
 
 class Flow(object):
     """Simple class for flows.
     Flows will trigger host behavior.
     """
-    def __init__(self, source, destination, amount):
+    def __init__(self, source, destination, amount, env):
         """ Constructor for Flow class
         """
         self.src = source
@@ -19,23 +18,26 @@ class Flow(object):
         self.cwnd = 1
         self.ssthresh = 10
         self.packets_sent = []
+        self.env = env
 
     def send_ack(self, packet):
         """ Creates ack based for packet.
         """
         if self.src == packet.dest and self.dest == packet.src:
             ack_packet = AckPacket(packet.pack_id, packet.dest, packet.src)
+            self.dest.send(ack_packet)
         else:
             print "Received wrong packet."
-        self.dest.send(ack_packet)
 
     def send_packet(self):
         """ Send a packet.
         """
-        pack = DataPacket(self.pack_num, self.src, self.dest)
-        self.packets_sent.append(self.pack_num)
-        self.src.send(pack)
-        self.pack_num = self.pack_num + 1
+        if self.env.time > self.flow_start and self.amount > 0:
+            pack = DataPacket(self.pack_num, self.src, self.dest)
+            self.packets_sent.append(self.pack_num)
+            self.src.send(pack)
+            self.pack_num = self.pack_num + 1
+            self.amount = self.amount - pack.size
 
     def receive(self, packet):
         """ Generate an ack or respond to bad packet.
