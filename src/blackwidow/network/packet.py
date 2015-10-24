9f3933be09eda254c87f7f3ab773274949eec943
Justin Leong <justinleong16@gmail.com>
2015-10-24 01:12:24 -0700

    Added flow_id to packet

--- src/blackwidow/network/packet.py
+++ src/blackwidow/network/packet.py
@@ -8,26 +8,27 @@ ACK_PACKET_SIZE = 64 * 8
 class Packet(object):
     """Super class for DataPackets and AckPackets"""
     
-    def __init__(self, packet_id, src, dest):
+    def __init__(self, packet_id, src, dest, flow_id):
         """Constructor for host class"""
         self.pack_id = packet_id
         self.src = src
-        self.dest = dest   
+        self.dest = dest
+        self.flow_id = flow_id
 
 class DataPacket(Packet):
     """Class for data packets"""
     
-    def __init__(self, packet_id, src, dest):
+    def __init__(self, packet_id, src, dest, flow_id):
         """Constructor for DataPacket class"""
-        super(DataPacket, self).__init__(packet_id, src, dest)
+        super(DataPacket, self).__init__(packet_id, src, dest, flow_id)
         self.size = DATA_PACKET_SIZE
         self.is_ack = False
 
 class AckPacket(Packet):
     """Class for acknowledgement packets"""
     
-    def __init__(self, packet_id, src, dest):
+    def __init__(self, packet_id, src, dest, flow_id):
         """Constructor for AckPackets class"""
-        super(AckPacket, self).__init__(packet_id, src, dest)
+        super(AckPacket, self).__init__(packet_id, src, dest, flow_id)
         self.size = ACK_PACKET_SIZE
         self.is_ack = True
