176f8887895569e37d0d97949cfb409b582d991c
Rahul Bachal <rbachal@caltech.edu>
2015-10-24 01:08:48 -0700

    Fixed debugging messages for link.py

--- src/blackwidow/network/link.py
+++ src/blackwidow/network/link.py
@@ -20,11 +20,11 @@ class Link():
     def receive(self, packet, source_id):
         # Add packet to link buffer as soon as it is received.
         # Drop packet if the buffer is full
-        print "I am link {0}. I have received packet {1}".format(self.id, packet.pack_id)
+        print "I am link {0}. I have received packet {1} at time {2}".format(self.id, packet.pack_id, self.env.time)
         if len(self.release_into_link_buffer) < self.capacity:
             self.release_into_link_buffer.appendleft(
                 [packet, source_id, self.env.time])
-            print "I am link {0}. I have successfully received the packet"
+            print "I am link {0}. I have successfully received the packet".format(self.id)
         else:
             print "Packet dropped."
 
