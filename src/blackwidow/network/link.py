cc37afd1cc86161f4b02de31bdfa8ff8e3222a86
Rahul Bachal <rbachal@caltech.edu>
2015-10-23 23:23:22 -0700

    Updated commenting for link.py

--- src/blackwidow/network/link.py
+++ src/blackwidow/network/link.py
@@ -18,13 +18,14 @@ class Link():
 
     def receive(self, packet, source_id):
         # Add packet to link buffer as soon as it is received.
+        # Drop packet if the buffer is full
         if len(release_into_link_buffer) < capacity:
             release_into_link_buffer.appendleft([packet, source_id, network.time])
 
     def send(self):
         # Release into link
         if (len(release_into_link_buffer) > 0):
-            # Peak at head
+            # Peek at head
             packet_info = release_into_link_buffer[-1]
             # Copy packet info fields.
             packet = packet_info[0]
@@ -33,9 +34,11 @@ class Link():
             # Check if packet has been sent by router.
             if (network.time - start_time >= packet.size / self.rate):
                 # Add it to queue of packets traveling through link.
+                # Update the current packet time to the send time
                 release_to_device_buffer.appendLeft([packet, source_id, network.time]);
+                # Remove current packet from bufer
                 release_into_link_buffer.pop()
-                # Update's packet time
+                # Update next packet time arrival time at front of queue
                 if (len(release_into_link_buffer) > 0):
                     release_into_link_buffer[-1][2] = network.time
 
@@ -48,8 +51,10 @@ class Link():
             start_time = packet_info[2]
             # Check if packet has arrived at end of link.
             if (network.time - start_time >= self.delay):
+                # Figure out which device to send to and send
                 if (source_id == device_a.network_id):
                     device_a.receive(packet)
                 elif (source_id == device_b.network_id):
                     device_b.receive(packet)
+                # Remove currenet packet from buffer
                 release_to_device_buffer.pop()
