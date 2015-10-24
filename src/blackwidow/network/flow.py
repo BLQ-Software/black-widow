395082f63b424b3e3d716f43a6533fdac47ae972
Justin Leong <justinleong16@gmail.com>
2015-10-24 01:59:36 -0700

    Added debugging stuff

--- src/blackwidow/network/flow.py
+++ src/blackwidow/network/flow.py
@@ -1,6 +1,7 @@
 """ Flow class
 """
 
+import pdb
 from blackwidow.network.packet import AckPacket, DataPacket
 
 class Flow(object):
@@ -15,7 +16,7 @@ class Flow(object):
         self.dest = destination
         self.amount = amount*8*10**6
         self.pack_num = 0
-        self.cwnd = 1
+        self.cwnd = 10
         self.ssthresh = 10
         self.packets_sent = []
         self.env = env
@@ -35,13 +36,14 @@ class Flow(object):
     def send_packet(self):
         """ Send a packet.
         """
-        if self.env.time > self.flow_start and self.amount > 0:
+        if self.env.time > self.flow_start and self.amount > 0 and self.env.time%100 == 0:
             pack = DataPacket(self.pack_num, self.src, self.dest, self.flow_id)
             self.packets_sent.append(self.pack_num)
             self.src.send(pack)
             self.pack_num = self.pack_num + 1
-            self.amount = self.amount - pack.size
             print "Packet {0} sent".format(self.pack_num)
+            print "{0} bits left".format(self.amount)
+            self.amount = self.amount - pack.size
         else:
             print "Waiting"
 
@@ -53,6 +55,7 @@ class Flow(object):
             print "Packet {0} received".format(self.pack_num)
             self.send_ack(packet)
         else:
+            pdb.set_trace()
             self.respond_to_ack()
             self.packets_sent.remove(packet.pack_id)
             print "Ack received for packet {0}".format(self.pack_num)
