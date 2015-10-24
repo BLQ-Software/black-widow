043d7fd93734952bc78971b69444c2d64ba3fb5a
Nancy Wen <nwen@caltech.edu>
2015-10-23 21:22:26 -0700

    Added a comment to packet class

--- src/blackwidow/network/packet.py
+++ src/blackwidow/network/packet.py
@@ -1,8 +1,10 @@
 #This file contains the Packet class and its two subclasses,
 #DataPacket and AckPacket.
 
+#Constant sizes of packets in bits
 DATA_PACKET_SIZE = 1024 * 8
 ACK_PACKET_SIZE = 64 * 8
+
 class Packet(object):
     """Super class for DataPackets and AckPackets"""
     
@@ -19,7 +21,7 @@ class DataPacket(Packet):
     """Constructor for DataPacket class"""
         super(Packet, self).__init__()
         self.size = DATA_PACKET_SIZE
-        self.ack = False
+        self.is_ack = False
 
 class AckPacket(Packet):
     """Class for acknowledgement packets"""
@@ -27,4 +29,4 @@ class AckPacket(Packet):
     def __init__(self, pack_id, src, dest):
         """Constructor for AckPackets class"""
         self.size = ACK_PACKET_SIZE
-        self.ack = True
+        self.is_ack = True
