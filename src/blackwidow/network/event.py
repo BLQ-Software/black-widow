0a6c8ccb32118634e0b1403a161a56983999f7a1
Rahul Bachal <rbachal@caltech.edu>
2015-11-07 12:45:31 -0800

    Removed time import from network. Added string repr to event

--- src/blackwidow/network/event.py
+++ src/blackwidow/network/event.py
@@ -2,7 +2,10 @@
 
 
 class Event:
+    id = 0
     def __init__(self, type, f, **kwargs):
+        self.id = Event.id
+        Event.id += 1
         self.type = type
         self.f = f
         self.kwargs = kwargs
@@ -10,4 +13,6 @@ class Event:
     def run(self):
         self.f(**self.kwargs)
 
-
+    def __str__(self):
+        msg = "Event {0} running: {1}"
+        return msg.format(self.id, self.type)
