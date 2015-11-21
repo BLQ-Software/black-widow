bb3b64d749d199367f882821a331958f6bf9cc02
Justin Leong <justinleong16@gmail.com>
2015-11-20 23:21:38 -0800

    Removed redundant code and made Reno extend Tahoe

--- src/blackwidow/network/reno_flow.py
+++ src/blackwidow/network/reno_flow.py
@@ -1,8 +1,8 @@
 from blackwidow.network.packet import AckPacket, DataPacket
 from event import Event
-from flow import Flow
+from tahoe_flow import TahoeFlow
 
-class RenoFlow(Flow):
+class RenoFlow(TahoeFlow):
     """ Implements TCP Reno.
     Adds Fast Retransmit and Fast Recovery
     Flows will trigger host behavior.
@@ -27,25 +27,12 @@ class RenoFlow(Flow):
     def __init__(self, flow_id, source, destination, amount, env, time, bw):
         """ Constructor for Flow class
         """
-        Flow.__init__(self, flow_id, source, destination, amount, env, time ,bw)
+        TahoeFlow.__init__(self, flow_id, source, destination, amount, env, time ,bw)
         self._packets_arrived = []
         self._packets_arrived = range(0,(int)(self._amount/(1024*8))) 
         self._total_num_pack = (int)(self._amount/(1024*8)) + 1
         self._last_pack_rec = -1
         self._counter = 0
-
-    def _send_ack(self, packet):
-        """ Creates ack based for packet.
-        """
-        if self._src == packet.src and self._dest == packet.dest:
-            next_ack_expected = self._total_num_pack
-            if len(self._packets_arrived) > 0:
-                next_ack_expected = self._packets_arrived[0]
-            ack_packet = AckPacket(packet.pack_id, packet.dest, packet.src, self._flow_id, next_ack_expected)
-            self._dest.send(ack_packet)
-            print "Flow sent ack packet {0}".format(packet.pack_id)
-        else:
-            print "Received wrong packet."
 
     def receive(self, packet):
         """ Generate an ack or respond to bad packet.
