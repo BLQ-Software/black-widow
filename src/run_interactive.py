6271cbb6ebf113e0d7e15b9dd59cdc17d5213d11
Rahul Bachal <rbachal@caltech.edu>
2015-11-22 13:05:47 -0800

    Interactive script works to make network

--- src/run_interactive.py
+++ src/run_interactive.py
@@ -1,3 +1,4 @@
+import os
 import cmd
 from blackwidow import BlackWidow
 from blackwidow.network import *
@@ -8,24 +9,61 @@ network = None
 class BlackWidowInteractive(cmd.Cmd):
 
     def do_create_network(self, line):
+        args = line.split()
+        if len(args) == 1:
+            self.filename = args[0]
+        else:
+            self.filename = "test.dot"
         self.network = Network()
         self.bw = BlackWidow()
 
     def do_add_router(self, line):
-        args = line.split(" ")
-        self.network.add_router(args[0], bw)
-        print "Router"
+        args = line.split()
+        if len(args) != 1:
+            print "*** invalid number of arguments"
+            return
+        try:
+            self.network.add_router(args[0], bw)
+        except Exception as e:
+            print e
 
     def do_add_host(self, line):
-        args = line.split(" ")
-        self.network.add_host(args[0])
-        print "Host"
+        args = line.split()
+        if len(args) != 1:
+            print "*** invalid number of arguments"
+            return
+        try:
+            self.network.add_host(args[0])
+        except Exception as e:
+            print e
 
     def do_add_link(self, line):
-        print "Link"
+        args = line.split()
+        if len(args) != 6:
+            print "*** invalid number of arguments"
+            return
+
+        try:
+            args.append(self.bw)
+            self.network.add_link(*args)
+        except Exception as e:
+            print e
 
     def do_add_flow(self, line):
-        print "Flow"
+        args = line.split()
+        if len(args) != 5:
+            print "*** invalid number of arguments"
+            return
+        try:
+            args.append(self.bw)
+            self.network.add_flow(*args)
+        except AttributeError:
+            print "*** network must be created first"
+
+    def do_show_network(self, line):
+        self.network.dump(self.filename)
+        os.system("dot -T png -Gdpi=3000 {0} > {1}.png ; open {1}.png".format(self.filename, self.filename[0:-4]))
+
 
     def do_EOF(self, line):
         print
