efca58520a4d4aa2ccc09d49e240a0e712dc65d2
Justin Leong <justinleong16@gmail.com>
2015-11-23 01:21:38 -0800

    Added file for FAST TCP implementation

---
+++ src/blackwidow/network/fast_flow.py
@@ -0,0 +1,96 @@
+from blackwidow.network.packet import AckPacket, DataPacket
+from event import Event
+from flow import Flow
+
+class FastFlow(Flow):
+    """ Implements FAST TCP.
+    Flows will trigger host behavior.
+
+    Parameters
+    ----------
+    flow_id : string
+        A unique id for the flow.
+    source : `Device`
+        The source for the flow.
+    destination : `Device`
+        The destination for the flow.
+    amount : int
+        The amount of data to send in MB.
+    env : `Network`
+        The network that the flow belongs to.
+    time : float
+        The amount of time to wait before starting to send in ms.
+
+    """
+    def __init__(self, flow_id, source, destination, amount, env, time, bw):
+        """ Constructor for Flow class
+        """
+        Flow.__init__(self, flow_id, source, destination, amount, env, time ,bw)
+        self._rtt = 100
+        self._rtt_min = 100
+        self._alpha = 1.0
+        self._wnd = 1.0
+
+    def _send_ack(self, packet):
+        """ Creates ack based for packet.
+        """
+        if self._src == packet.src and self._dest == packet.dest:
+            next_ack_expected = self._total_num_pack
+            if len(self._packets_arrived) > 0:
+                next_ack_expected = self._packets_arrived[0]
+            ack_packet = AckPacket(packet.pack_id, packet.dest, packet.src, self._flow_id, next_ack_expected)
+            self._dest.send(ack_packet)
+            print "Flow sent ack packet {0}".format(packet.pack_id)
+        else:
+            print "Received wrong packet."
+
+    def receive(self, packet):
+        """ Generate an ack or respond to bad packet.
+
+        Parameters
+        ----------
+        packet : `Packet`
+            The packet to be received.
+
+        """
+        if packet.dest == self._dest:
+            print "Flow received packet {0}".format(packet.pack_id)
+            if packet.pack_id in self._packets_arrived:
+                self._packets_arrived.remove(packet.pack_id)
+            self._send_ack(packet)
+        else:
+            # Check for duplicate acknowledgements
+            if packet.next_expected == self._last_pack_rec:
+                self._counter = self._counter + 1
+            else:
+                if self._counter >= 3:
+                    # window deflation on non-dup ACK
+                    self._cwnd = self._ssthresh
+                    print "Flow {} window size is {} - fast retransmit".format(self._flow_id, self._cwnd)
+                    self.bw.record('{0}, {1}'.format(self.env.time, self._cwnd), 'flow_{0}.window'.format(self.flow_id))
+                self._counter = 0
+                self._last_pack_rec = packet.next_expected
+            # Fast retransmit/Fast recovery
+            if self._counter == 3:
+                if self._cwnd > 4:
+                    self._ssthresh = self._cwnd/float(2)
+                else:
+                    self._ssthresh = 2
+                # Go back n
+                self._pack_num = packet.next_expected
+                # window inflation where ndup = 3
+                self._cwnd = self._ssthresh + self._counter
+                self.env.add_event(Event("Resend", self.send_packet), 10)
+                if packet.next_expected not in self._packets_time_out:
+                    self._packets_time_out.append(packet.next_expected)
+                print "Flow {} window size is {} - fast retransmit".format(self._flow_id, self._cwnd)
+                self.bw.record('{0}, {1}'.format(self.env.time, self._cwnd), 'flow_{0}.window'.format(self.flow_id))
+            if packet.pack_id not in self._acks_arrived:
+                self._respond_to_ack()
+                if packet.pack_id in self._packets_sent:
+                    self._packets_sent.remove(packet.pack_id)
+                if packet.pack_id in self._packets_time_out:
+                    self._packets_time_out.remove(packet.pack_id)
+                self._acks_arrived.add(packet.pack_id)
+                if len(self._packets_sent) + len(self._acks_arrived) == 0:
+                    self.env.decrement_flows()
