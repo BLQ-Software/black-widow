506700f251664d5efccd0cfaafe81393edc099b4
Justin Leong <justinleong16@gmail.com>
2015-11-29 22:20:04 -0800

    Fixed bug with flow decrementing twice

--- src/blackwidow/graph/graph_rate.py
+++ src/blackwidow/graph/graph_rate.py
@@ -182,33 +182,33 @@ class CsvGrapher(object):
         fig = plt.figure(1, figsize=(15,8))
 
         for flow in flows:
-            window_size_path = '{}/{}.flow {} receive rate.csv'.format(data_dir, log_file, flow[0])
-            packet_delay_path = '{}/{}.flow {} send rate.csv'.format(data_dir, log_file, flow[0])
+            receive_rate_path = '{}/{}.flow {} receive rate.csv'.format(data_dir, log_file, flow[0])
+            send_rate_path = '{}/{}.flow {} send rate.csv'.format(data_dir, log_file, flow[0])
 
-            if (os.path.isfile(window_size_path)):
+            if (os.path.isfile(receive_rate_path)):
                 # Load in send rate data
-                window_size = np.genfromtxt(window_size_path, delimiter=',')
-                window_size = window_size.astype(int)
-                window_size_times = window_size[:,0]
-                window_size = window_size[:,1]
+                receive_rate = np.genfromtxt(receive_rate_path, delimiter=',')
+                receive_rate = receive_rate.astype(int)
+                receive_rate_times = receive_rate[:,0]
+                receive_rate = receive_rate[:,1]
 
                 # Plot the send rate
                 plt.subplot(2, 1, 1)
-                plt.plot(window_size_times, window_size, markersize=5, label=flow[0])
+                plt.plot(receive_rate_times, receive_rate, markersize=5, label=flow[0])
                 plt.legend()
                 plt.xlabel('time (ms)', fontsize=18)
                 plt.ylabel('receive rate (bits/ms)', fontsize=18)
 
-            if (os.path.isfile(packet_delay_path)):
+            if (os.path.isfile(send_rate_path)):
                 # Load in receive rate data
-                packet_delay = np.genfromtxt(packet_delay_path, delimiter=',')
-                packet_delay = packet_delay.astype(int)
-                packet_delay_times = packet_delay[:,0]
-                packet_delay = packet_delay[:,1]
+                send_rate = np.genfromtxt(send_rate_path, delimiter=',')
+                send_rate = send_rate.astype(int)
+                send_rate_times = send_rate[:,0]
+                send_rate = send_rate[:,1]
 
                 # Plot the receive rate 
                 plt.subplot(2, 1, 2)
-                plt.plot(packet_delay_times, packet_delay, markersize=5, label=flow[0])
+                plt.plot(send_rate_times, send_rate, markersize=5, label=flow[0])
                 plt.legend()
                 plt.xlabel('time (ms)', fontsize=18)
                 plt.ylabel('send rate (bits/ms)', fontsize=18)
