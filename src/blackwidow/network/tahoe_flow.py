37e90d1aa01c2f55a8174c454972d2e1ed368de4
Justin Leong <justinleong16@gmail.com>
2015-12-09 15:36:24 -0800

    Removed extraneous attributes from Tahoe

--- src/blackwidow/network/tahoe_flow.py
+++ src/blackwidow/network/tahoe_flow.py
@@ -75,9 +75,5 @@ class TahoeFlow(Flow):
         """ Constructor for Flow class
         """
         Flow.__init__(self, flow_id, source, destination, amount, env, time ,bw)
-        self._ssthresh = 1000
-        self._packets_arrived = []
-        self._packets_arrived = range(0,(int)(self._amount/(1024*8)))
-        self._total_num_pack = (int)(self._amount/(1024*8)) + 1
-        self._last_pack_rec = -1
-        self._counter = 0
+        self._ssthresh = 9999
+        self._resend_time = 100
