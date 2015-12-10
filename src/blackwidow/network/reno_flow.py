3eaaf6d199362a21779c1dae34ac2187d922591e
Rahul Bachal <rbachal@caltech.edu>
2015-12-09 17:13:31 -0800

    Reno Flow PEP8

--- src/blackwidow/network/reno_flow.py
+++ src/blackwidow/network/reno_flow.py
@@ -2,6 +2,7 @@ from blackwidow.network.packet import AckPacket, DataPacket
 from event import Event
 from tahoe_flow import TahoeFlow
 
+
 class RenoFlow(TahoeFlow):
     """ Implements TCP Reno.
     Adds Fast Retransmit and Fast Recovery
@@ -64,34 +65,37 @@ class RenoFlow(TahoeFlow):
     acks_arrived : set
         Set of ack packets that have been received
     done : int
-        0 if flow isn't finished; 1 if flow is finished; used to avoid decrementing flow more than once
+        0 if flow isn't finished; 1 if flow is finished
+        Used to avoid decrementing flow more than once.
     send_rate : Rate_Graph
-        Keeps track of the rate the flow is sending at and outputs to CSV file in real time
+        Keeps track of the rate the flow is sending at and outputs to CSV file
+        in real time.
     receive_rate : Rate_Graph
-        Keeps track of the rate the flow is receiving at and outputs to CSV file in real time 
+        Keeps track of the rate the flow is receiving at and outputs to CSV
+        file in real time.
     packets_arrived : list
-        Keeps track of packets(not acks) that have not arrived. Filled with all possible
-        packet numbers when the flow starts and numbers are removed as each packet reaches
-        the destination
+        Keeps track of packets(not acks) that have not arrived. Filled with all
+        possible packet numbers when the flow starts and numbers are removed as
+        each packet reaches the destination
     total_num_packets : int
         Total number of packets that need to be sent
     last_pack_rec : int
-        Packet number of previous next packet expected by the destination 
+        Packet number of previous next packet expected by the destination
     counter : int
         Keeps track of duplicate acknowledgements
     """
     def __init__(self, flow_id, source, destination, amount, env, time, bw):
         """ Constructor for Flow class
         """
-        TahoeFlow.__init__(self, flow_id, source, destination, amount, env, time ,bw)
+        TahoeFlow.__init__(self, flow_id, source, destination, amount, env,
+                           time, bw)
         self._ssthresh = 1000
         self._packets_arrived = []
-        self._packets_arrived = range(0,(int)(self._amount/(1024*8)))
+        self._packets_arrived = range(0, (int)(self._amount/(1024*8)))
         self._total_num_pack = (int)(self._amount/(1024*8)) + 1
         self._last_pack_rec = -1
         self._counter = 0
         self._resend_time = 10
-
 
     def _send_ack(self, packet):
         """ Creates ack for packet.
@@ -101,7 +105,9 @@ class RenoFlow(TahoeFlow):
                 next_ack_expected = self._packets_arrived[0]
             else:
                 next_ack_expected = self._total_num_pack
-            ack_packet = AckPacket(packet.pack_id, packet.dest, packet.src, self._flow_id, next_ack_expected, timestamp=packet.timestamp)
+            ack_packet = AckPacket(packet.pack_id, packet.dest, packet.src,
+                                   self._flow_id, next_ack_expected,
+                                   timestamp=packet.timestamp)
             self._dest.send(ack_packet)
             print "Flow sent ack packet {0}".format(packet.pack_id)
         else:
@@ -127,8 +133,12 @@ class RenoFlow(TahoeFlow):
                 if self._counter >= 3:
                     # window deflation on non-dup ACK
                     self._cwnd = self._ssthresh
-                    print "Flow {} window size is {} - fast retransmit".format(self._flow_id, self._cwnd)
-                    self.bw.record('{0}, {1}'.format(self.env.time, self._cwnd), 'flow_{0}.window'.format(self.flow_id))
+                    print ("Flow {} window size is {} -"
+                           " fast retransmit".format(self._flow_id,
+                                                     self._cwnd))
+                    self.bw.record('{0}, {1}'.format(self.env.time,
+                                                     self._cwnd),
+                                   'flow_{0}.window'.format(self.flow_id))
                 self._counter = 0
                 self._last_pack_rec = packet.next_expected
             # Fast retransmit/Fast recovery
@@ -143,9 +153,14 @@ class RenoFlow(TahoeFlow):
                 self._cwnd = self._ssthresh + self._counter
                 if packet.next_expected not in self._packets_time_out:
                     self._packets_time_out.append(packet.next_expected)
-                self.env.add_event(Event("Resend", self._flow_id, self.send_packet), 100)
-                print "Flow {} window size is {} - fast retransmit".format(self._flow_id, self._cwnd)
-                self.bw.record('{0}, {1}'.format(self.env.time, self._cwnd), 'flow_{0}.window'.format(self.flow_id))
+                self.env.add_event(Event("Resend",
+                                         self._flow_id,
+                                         self.send_packet),
+                                   100)
+                print ("Flow {} window size is {} -"
+                       " fast retransmit".format(self._flow_id, self._cwnd))
+                self.bw.record('{0}, {1}'.format(self.env.time, self._cwnd),
+                               'flow_{0}.window'.format(self.flow_id))
             self._receive_ack(packet)
 
     def _reset_window(self):
