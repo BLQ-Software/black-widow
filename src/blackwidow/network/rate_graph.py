7fedfdd33c2290585457ad600ff15d771395c9a1
Rahul Bachal <rbachal@caltech.edu>
2015-12-09 17:09:10 -0800

    Rate_Graph PEP8

--- src/blackwidow/network/rate_graph.py
+++ src/blackwidow/network/rate_graph.py
@@ -2,6 +2,7 @@ from blackwidow.network.packet import Packet
 from event import Event
 from Queue import PriorityQueue
 
+
 class Data(object):
     """ Class to represent an amount of data and the time it was sent.
     It is used as a priority queue object with time as the priority.
@@ -44,6 +45,7 @@ class Data(object):
         """Method to compare with other objects in the priority queue."""
 
         return cmp(self._time, other.time)
+
 
 class Rate_Graph(object):
     """ Class to graph rates.
@@ -92,7 +94,10 @@ class Rate_Graph(object):
         self.bits_in_window = 0
         # Interval between points
         self.interval = 10
-        self.env.add_event(Event("Graph rate", self.object_id, self.graph), self.window_size)
+        self.env.add_event(Event("Graph rate",
+                                 self.object_id,
+                                 self.graph),
+                           self.window_size)
 
     def add_point(self, packet, time):
         """ Adds a point to the queue
@@ -124,11 +129,14 @@ class Rate_Graph(object):
         self.window.put(temp)
         return temp.time
 
-
     def graph(self):
         """ Graphs current rate
         """
         self.remove_points(self.env.time-self.window_size)
         current_rate = float(self.bits_in_window)/float(self.window_size)
-        self.bw.record('{0}, {1}'.format(self.env.time, current_rate), '{0}'.format(self.name))
-        self.env.add_event(Event("Graph rate", self.object_id, self.graph), self.interval)
+        self.bw.record('{0}, {1}'.format(self.env.time, current_rate),
+                       '{0}'.format(self.name))
+        self.env.add_event(Event("Graph rate",
+                                 self.object_id,
+                                 self.graph),
+                           self.interval)
